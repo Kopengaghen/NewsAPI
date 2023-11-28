577d092109c9bd4b6c90260c3781ee357242c291
Mihai <trofimciuc.m@gmail.com>
2023-11-27 22:36:12 -0500

    Added loading data

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ function App() {
     const [news, setNews] = useState([])
     const [searchQuery, setSearchQuery] = useState("react")
     const [url, setUrl] = useState("http://hn.algolia.com/api/v1/search?query=react")
+    const [loading, setLoading] = useState(false)
 
     const posts = () => {
+        setLoading(true)
         fetch(url)
             .then(result => result.json())
-            .then(data => setNews(data.hits))
+            .then(data => (setNews(data.hits),
+                setLoading(false)))
+            .catch(error => console.log(error))
     }
 
     useEffect( () => {
@@ -27,6 +31,7 @@ function App() {
     return (
         <div>
             <h2>News</h2>
+            {loading ? <h1>Loading...</h1> : ""}
             <form onSubmit={handleSubmit} >
                 <input type="text" value={searchQuery} onChange={handleChange}/>
                 <button>Search</button>
