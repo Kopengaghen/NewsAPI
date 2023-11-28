faed5679f0fa5a976b1a4bee0f84a7c76eb41e2f
Mihai <trofimciuc.m@gmail.com>
2023-11-27 22:13:38 -0500

    Added useState(news, searchQuery and url) and useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div>
-      <h2>News</h2>
-    </div>
-  );
+    const [news, setNews] = useState([])
+    const [searchQuery, setSearchQuery] = useState("react")
+    const [url, setUrl] = useState("http://hn.algolia.com/api/v1/search?query=react")
+
+    const posts = () => {
+        fetch(url)
+            .then(result => result.json())
+            .then(data => setNews(data.hits))
+    }
+
+    useEffect( () => {
+        posts()
+        }, [url])
+
+    const handleChange = (e) => {
+        setSearchQuery(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+    }
+
+    return (
+        <div>
+            <h2>News</h2>
+            <form onSubmit={handleSubmit} >
+                <input type="text" value={searchQuery} onChange={handleChange}/>
+                <button>Search</button>
+            </form>
+            {news.map((item, index) => <p key={index}>{item.title}</p>)}
+        </div>
+    );
 }
 
 export default App;
